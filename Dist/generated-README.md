f94821478290fb69c5927c9650eb4363f94c1177
Cristian Trigueros <cristiantrigueros@Cristians-MBP.lan>
2021-11-15 10:24:09 -0600

    additional commit

---
+++ Dist/generated-README.md
@@ -0,0 +1,49 @@
+
+    # Readme Generator
+    
+    
+    ![badge](https://img.shields.io/badge/license-mit-blue)
+      
+    ## Table-of-Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    
+    * [License](#license)
+      
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## [Description](#table-of-contents)
+    create a generator 
+    kakdjfa
+    test
+    ## [Installation](#table-of-contents)
+    test
+    ## [Usage](#table-of-contents)
+    test
+    
+    For more information on how to add screenshots for examples, visit the following website:
+    
+    [Mark Down Tutorial](https://agea.github.io/tutorial.md/)
+    
+    
+    ## [License](#table-of-contents)
+    The application is covered under the following license:
+    
+    [mit](https://choosealicense.com/licenses/mit)
+      
+      
+    ## [Contributing](#table-of-contents)
+    
+    
+    Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+      
+    ## [Tests](#table-of-contents)
+    test
+    ## [Questions](#table-of-contents)
+    Please contact me using the following links:
+    [GitHub](https://github.com/cristrig)
+    [Email: cbtirug@gkajkd.com](mailto:cbtirug@gkajkd.com)
+  
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
+// Packages needed for this application
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/Generatermarkdown.js");
 const fs = require("fs");
-const generateMarkdown = require("./utils/generateMarkdown.js");
-
-// array of questions for user
+// Array of questions for user input
 const questions = [
   {
     type: "input",
     name: "title",
-    message: "Please provide a project title.  (Required)",
-    validate: (nameInput) => {
-      if (nameInput) {
+    message: "What is the title of your project? (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
         return true;
       } else {
-        console.log("Please provide a project title!");
+        console.log("Please enter your title!");
         return false;
       }
     },
   },
   {
     type: "input",
-    name: "github",
-    message: "Please enter your GitHub username. (Required)",
+    name: "githubUsername",
+    message: "What is your GitHub Username? (Required)",
     validate: (githubInput) => {
       if (githubInput) {
         return true;
@@ -32,103 +32,52 @@ const questions = [
   },
   {
     type: "input",
-    name: "repo",
-    message: "Please enter the name of your repo. (Required)",
-    validate: (repoInput) => {
-      if (repoInput) {
+    name: "email",
+    message: "What is your email address? (Required)",
+    validate: (githubInput) => {
+      if (githubInput) {
         return true;
       } else {
-        console.log("Please enter the name of your repo!");
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Provide a description of your application. (Required)",
-    validate: (descInput) => {
-      if (descInput) {
-        return true;
-      } else {
-        console.log("Please enter a description!");
+        console.log("Please enter your email address!");
         return false;
       }
     },
   },
   {
     type: "input",
-    name: "usage",
-    message:
-      "Please provide information for using your application. (Required)",
-    validate: (usageInput) => {
-      if (usageInput) {
+    name: "what",
+    message: "What is your project and what problem will it solve? (Required)",
+    validate: (whatInput) => {
+      if (whatInput) {
         return true;
       } else {
-        console.log("Please provide information for using your application!");
+        console.log("Please enter what your project is!");
         return false;
       }
     },
-  },
-  {
-    type: "checkbox",
-    name: "contents",
-    message:
-      "Any additional sections you would like to include in your README?",
-    choices: [
-      {
-        name: "Deployed Application",
-        checked: false,
-      },
-      {
-        name: "Installation",
-        checked: false,
-      },
-      {
-        name: "Screenshots",
-        checked: true,
-      },
-      {
-        name: "Built With",
-        checked: true,
-      },
-      {
-        name: "License",
-        checked: false,
-      },
-      {
-        name: "Contributing",
-        checked: false,
-      },
-      {
-        name: "Tests",
-        checked: false,
-      },
-      {
-        name: "Questions",
-        checked: true,
-      },
-      {
-        name: "Credits",
-        checked: true,
-      },
-    ],
   },
   {
     type: "input",
-    name: "link",
-    message: "Please provide a link to your deployed application.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Deployed Application") > -1) {
+    name: "why",
+    message: "Why did you create this project? (Required)",
+    validate: (whyInput) => {
+      if (whyInput) {
         return true;
       } else {
+        console.log("Please enter why you created this project!");
         return false;
       }
     },
-    validate: (linkInput) => {
-      if (linkInput) {
+  },
+  {
+    type: "input",
+    name: "how",
+    message: "How will someone use this? (Required)",
+    validate: (howInput) => {
+      if (howInput) {
         return true;
       } else {
-        console.log("Please enter a link!");
+        console.log("Please enter what your project is!");
         return false;
       }
     },
@@ -137,19 +86,25 @@ const questions = [
     type: "input",
     name: "installation",
     message:
-      "Please list any required packages for installation of your application.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Installation") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
+      "Please provide step-by-step installation instructions for your project. (Required)",
     validate: (installInput) => {
       if (installInput) {
         return true;
       } else {
-        console.log("Please enter installation instructions!");
+        console.log("Please enter your installation instructions!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide instructions and examples for use. (Required)",
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log("Please enter your use instructions!");
         return false;
       }
     },
@@ -157,255 +112,86 @@ const questions = [
   {
     type: "list",
     name: "license",
-    message: "Please provide license information.",
-    choices: ["MIT", "GNU", "Apache 2.0", "ISC"],
-    default: 0,
-    when: ({ contents }) => {
-      if (contents.indexOf("License") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    validate: (licenseInput) => {
-      if (licenseInput) {
-        return true;
-      } else {
-        console.log("Please provide license information!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "checkbox",
-    name: "built with",
-    message:
-      "Please select the technologies that your application was built with.",
-    choices: ["HTML", "CSS", "SASS", "JavaScript", "Node.js", "Express.js"],
-    default: 0,
-    when: ({ contents }) => {
-      if (contents.indexOf("Built With") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "Please enter your guidelines for contributing.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Contributing") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    validate: (contributingInput) => {
-      if (contributingInput) {
-        return true;
-      } else {
-        console.log("Please enter guidelines for contributing!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "Please enter test information for your application.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Tests") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    validate: (testsInput) => {
-      if (testsInput) {
-        return true;
-      } else {
-        console.log(
-          "What packages are required to run tests for your application?"
-        );
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "questions",
-    message:
-      "Please provide an email address for others to reach you with questions.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Questions") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    validate: (questionsInput) => {
-      if (questionsInput) {
-        return true;
-      } else {
-        console.log("Please provide an email address!");
-        return false;
-      }
-    },
-  },
-];
-// array of prompts for adding screenshots
-const screenshotQues = [
-  {
-    type: "input",
-    name: "screenshotLink",
-    message: "Please provide a link for your screenshot. (Required)",
-    validate: (screenshotLinkInput) => {
-      if (screenshotLinkInput) {
-        return true;
-      } else {
-        console.log("Please provide a link for your screenshot!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "screenshotAlt",
-    message: "Please provide alt text for your screenshot. (Required)",
-    validate: (screenshotAltInput) => {
-      if (screenshotAltInput) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "screenshotDesc",
-    message: "Please provide a description of your screenshot. (Optional)",
+    message: "Which license will you use for your project?",
+    choices: ["agpl", "apache", "mit", "no license"],
   },
   {
     type: "confirm",
-    name: "confirmAddScreenshot",
-    message: "Would you like to add another screenshot?",
-    default: false,
+    name: "confirmContributers",
+    message: "Would you like to allow other developers to contribute?",
+    default: true,
   },
-];
-// array of prompts for adding credits
-const creditQues = [
   {
     type: "input",
-    name: "creditName",
-    message: "Please give your credit a name. (Required)",
-    validate: (creditName) => {
-      if (creditName) {
+    name: "contribute",
+    message: "Please provide guidelines for contributing. (Required)",
+    when: ({ confirmContributers }) => {
+      if (confirmContributers) {
         return true;
       } else {
-        console.log("Please enter a name for the credit!");
+        return false;
+      }
+    },
+    validate: (contributerInput) => {
+      if (contributerInput) {
+        return true;
+      } else {
+        console.log("Please enter contributer guidelines!");
         return false;
       }
     },
   },
   {
     type: "input",
-    name: "creditLink",
-    message: "Please provide a link for the credit.  (Required)",
-    validate: (creditLink) => {
-      if (creditLink) {
+    name: "test",
+    message: "Please provide instructions on how to test the app. (Required)",
+    validate: (testInput) => {
+      if (testInput) {
         return true;
       } else {
-        console.log("Please enter a name for the credit!");
+        console.log("Please enter your use test instructions!");
         return false;
       }
     },
   },
-  {
-    type: "confirm",
-    name: "confirmAddCredit",
-    message: "Would you like to add another credit?",
-    default: false,
-  },
 ];
-// recursive function for adding screenshots
-addScreenshots = (readmeData) => {
-  // initiates screenshot array
-  if (!readmeData.screenshots) {
-    readmeData.screenshots = [];
-  }
-  console.log(`
-==================
-Add New Screenshot
-==================
-    `);
-  return inquirer.prompt(screenshotQues).then((screenshotData) => {
-    // adds the screenshot to the array
-    readmeData.screenshots.push(screenshotData);
-    // will call addScreenshots again based on user input
-    if (screenshotData.confirmAddScreenshot) {
-      return addScreenshots(readmeData);
-    } else {
-      return readmeData;
-    }
-  });
-};
-// recursive function for adding credits
-addCredits = (readmeInfo) => {
-  // initiates array for credits
-  if (!readmeInfo.credits) {
-    readmeInfo.credits = [];
-  }
-  console.log(`
-==============
-Add New Credit
-==============
-    `);
-  return inquirer.prompt(creditQues).then((creditData) => {
-    // adds credits to array
-    readmeInfo.credits.push(creditData);
-    // will call addCredits again based on user input
-    if (creditData.confirmAddCredit) {
-      return addCredits(readmeInfo);
-    } else {
-      return readmeInfo;
-    }
-  });
-};
+
 // function to write README file
-function writeToFile(fileName, data) {
-  fs.writeFile(`./dist/${fileName}`, data, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("README created!");
+const writeFile = (fileContent) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./dist/generated-README.md", fileContent, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: "File created!",
+      });
+    });
   });
-}
-// function to initialize program
-function init() {
-  return inquirer.prompt(questions);
-}
-// function call to initialize program
+};
+
+// function to prompt questions and store user inputs
+const init = () => {
+  return inquirer.prompt(questions).then((readmeData) => {
+    return readmeData;
+  });
+};
+
+// Function call to initialize app
 init()
-  .then((userResponse) => {
-    // calls function to add screenshots based on user selection
-    if (userResponse.contents.indexOf("Screenshots") > -1) {
-      return addScreenshots(userResponse);
-    } else {
-      return userResponse;
-    }
+  .then((readmeData) => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
   })
-  .then((response) => {
-    // calls function to add credits based on user selection
-    if (response.contents.indexOf("Credits") > -1) {
-      return addCredits(response);
-    } else {
-      return response;
-    }
+  .then((pageMD) => {
+    return writeFile(pageMD);
   })
-  .then((answers) => generateMarkdown(answers))
-  .then((generatedReadme) => writeToFile("README.md", generatedReadme))
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse.message);
+  })
   .catch((err) => {
     console.log(err);
   });
